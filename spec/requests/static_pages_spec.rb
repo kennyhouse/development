a08aec8529c34e88a44dd29bdb246d4ed42ef162
kennyhouse <kennyhouse@gmail.com>
2014-07-11 00:21:13 -0700

    more change for title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper'
 describe "Static pages" do
 
 	describe "Home page" do
-	  it "should have the content 'Development App'" do
+	  it "should have the h1 'Development App'" do
 	  	visit '/static_pages/home'
 	  	page.should have_selector('h1', :text => 'Development App')
 	  end
 
-	  it "should have the title 'Home'" do
+	  it "should have the base title 'Home'" do
 	  	visit '/static_pages/home'
-	  	page.should have_selector('title', :text => "Ruby on Rails Tutorial Development App | Home")
+	  	page.should have_selector('title', :text => "Ruby on Rails Tutorial Development App")
+	  end
+
+	  it "should have a custom page title" do
+	  	visit '/static_pages/home'
+	  	page.should_not have_selector('title', :text => '| Home')
 	  end
 	end
 
