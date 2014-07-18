b898b57fc08bfd333fa911508273484a52728ab5
kennyhouse <kennyhouse@gmail.com>
2014-07-17 23:40:07 -0700

    changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,29 +4,29 @@ describe "Static pages" do
 
 	describe "Home page" do
 	  it "should have the h1 'Development App'" do
-	  	visit '/static_pages/home'
+	  	visit root_path
 	  	page.should have_selector('h1', :text => 'Development App')
 	  end
 
 	  it "should have the base title 'Home'" do
-	  	visit '/static_pages/home'
+	  	visit root_path
 	  	page.should have_selector('title', :text => "Ruby on Rails Tutorial Development App")
 	  end
 
 	  it "should have a custom page title" do
-	  	visit '/static_pages/home'
+	  	visit root_path
 	  	page.should_not have_selector('title', :text => '| Home')
 	  end
 	end
 
 	describe "Help page" do
 		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 
 		it "should have the title 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Development App | Help")
 		end
 	end
@@ -35,19 +35,24 @@ describe "Static pages" do
 
 	describe "About page" do
 		it "should have the h1 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('h1', :text => 'About')
 		end
 
 		it "should have the title 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Development App | About Us")
 		end
 	end
 
 	describe "Contact page" do
 		it "should have the h1 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
+			page.should have_selector('title', :text => 'Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit contact_path
 			page.should have_selector('title', :text => "Ruby on Rails Tutorial Development App | Contact")
 		end
 	end
